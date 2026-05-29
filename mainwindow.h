20e517909730b408469b208dbe3e902923044114
Kim <kim413455@gmail.com>
2026-05-29 17:32:16 +0800

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMap>
+#include <QTableWidgetItem>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void setUsername(const QString &name);
+
+private slots:
+    void on_addButton_clicked();
+    void on_removeButton_clicked();
+    void on_editButton_clicked();
+    void on_tableWidget_itemChanged(QTableWidgetItem *item);
+    void on_logoutButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QString itemNames[999];
+    int itemQty[999];
+    int itemCount =0;
+
+    void updateTable();
+};
+
+#endif // MAINWINDOW_H
